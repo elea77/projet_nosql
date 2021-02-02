6748683871f70689543c56b1a24f179015e1ca0e
SoniaFCE <56359360+SoniaFCE@users.noreply.github.com>
2021-02-02 18:54:14 +0100

    exposition

--- exposition.py
+++ exposition.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, render_template
+import db
 
 exposition = Blueprint("exposition", __name__, static_folder="static", template_folder="templates")
 
 @exposition.route("/expositions")
 def expositions():
-    return "Page des expositions !"
+    
+    exposition = db.db.api.find({"fields.category": { "$regex": '^expositions' }}, {"fields.title":1, "fields.category": 1, "fields.date_end": 1, "fields.cover_url":1}).limit(10)
+    print(expositions)
+    return render_template('exposition.html', expositions=exposition)
