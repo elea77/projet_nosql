208abd3264d77ad1c3130b17e93c67d348c4a23f
YaminaLouisThomas <yamina.gherbi@ynov.com>
2021-02-02 19:22:00 +0100

    expositionNew

--- exposition.py
+++ exposition.py
@@ -6,6 +6,7 @@ exposition = Blueprint("exposition", __name__, static_folder="static", template_
 @exposition.route("/expositions")
 def expositions():
     
-    exposition = db.db.api.find({"fields.category": { "$regex": '^expositions' }}, {"fields.title":1, "fields.category": 1, "fields.date_end": 1, "fields.cover_url":1}).limit(10)
+    exposition = db.db.api.find({"fields.category": { "$regex": '^Expositions' }}, {"fields.title":1, "fields.category": 1, "fields.date_end": 1, "fields.cover_url":1}).limit(10)
     print(expositions)
     return render_template('exposition.html', expositions=exposition)
+
